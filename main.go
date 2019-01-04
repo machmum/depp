fefd93fffc1c8c5755d5b416406f660a15c7da07
machmum <machmum@berrybenka.com>
2019-01-04 15:37:17 +0700

    update main

--- main.go
+++ main.go
@@ -1,56 +1,20 @@
 package main
 
 import (
-	"github.com/labstack/echo"
-	"github.com/labstack/gommon/log"
-	"fmt"
-	"github.com/labstack/echo/middleware"
-	"github.com/sirupsen/logrus"
-	"net/http"
-	"github.com/go-playground/validator"
-	"time"
+	"github.com/machmum/depp/config"
+	"github.com/machmum/depp/api"
 )
 
-func main(){
-	e := echo.New()
+func main() {
 
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}\n",
-	}))
+	path := "./config.local.yaml"
 
-	e.GET("/index", func(c echo.Context) (err error) {
-		return c.JSON(http.StatusOK, true)
-	})
-
-	logrus.WithFields(log.Fields{
-		"at": time.Now().Format("2006-01-02 15:04:05"),
-	})
-
-	e.Logger.Fatal(e.Start(":9000"))
-}
-
-// CustomValidator holds custom validator
-type CustomValidator struct {
-	V *validator.Validate
-}
-
-// Validate validates the request
-func (cv *CustomValidator) Validate(i interface{}) error {
-	return cv.V.Struct(i)
-}
-
-func AuthAPI() echo.MiddlewareFunc {
-	//func AuthAPI(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			//c.Error(errors.New("error"))
-			//return next(c)
-
-			fmt.Println("something 1")
-			//return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": "unauthorized hehe"})
-			return next(c)
-			//return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": "unauthorized 2"})
-		}
+	// get config
+	cfg, err := config.Load(path)
+	if err != nil {
+		panic(err.Error())
 	}
 
+	// run engine
+	api.Start(cfg)
 }
